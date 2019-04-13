52184e3a44d39f2987958bde8ce72d6b89040f71
Morgan <mmuse191@gmail.com>
2019-04-13 16:42:13 -0500

    made it to where you can get all users as admin

--- routes/users.js
+++ routes/users.js
@@ -14,8 +14,14 @@ router.get("/me", auth, async (req, res) => {
   res.send(user);
 });
 
+// get all users
+router.get("/", auth, async (req, res) => {
+  const users = await User.find().sort("name");
+  res.send(users);
+});
+
 // add a new user
-router.post("/", async (req, res) => {
+router.post("/signup", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -32,5 +38,26 @@ router.post("/", async (req, res) => {
     .header("x-auth-token", token)
     .send(_.pick(user, ["_id", "name", "email"]));
 });
+
+// // update a user
+// router.put("/:id", auth, async (req, res) => {
+//   const { error } = validate(req.body);
+//   if (error) return res.status(400).send(error.details[0].message);
+
+//   const user = await User.findByIdAndUpdate(
+//     req.params.id,
+//     {
+//       name: req.body.name,
+//       email: req.body.email,
+//       passowrd: req.body.password
+//     },
+//     { new: true }
+//   );
+
+//   if (!user)
+//     return res.status(404).send("The user with the given ID was not found");
+
+//   res.send(user);
+// });
 
 module.exports = router;
